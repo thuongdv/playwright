286d128a329090cfc5099d0bbd98f6db36e77485
Thuong Dang <thuong.dang@logigear.com>
2024-01-15 15:09:20 +0700

    Implement negative test

--- tests/tadashboard/login.spec.ts
+++ tests/tadashboard/login.spec.ts
@@ -1,12 +1,23 @@
-import { test } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 import DashboardMainPage from 'pages/dashboard-main-page';
 import LoginPage from 'pages/login-page';
 
-test('Verify that user can login specific repository successfully via Dashboard login page with correct credentials', async ({ page }) => {
+test('Verify that user can login specific repository successfully via Dashboard login page with correct credentials @SmokeTest', async ({ page }) => {
   const loginPage = new LoginPage(page);
   await loginPage.go();
   await loginPage.login('administrator', '');
 
   const dashboardMainPage = new DashboardMainPage(page);
   await dashboardMainPage.displays();
+})
+
+test('Verify that user fails to login specific repository successfully via Dashboard login page with incorrect credentials', async ({ page }) => {
+  const loginMessage = 'Username or password is invalid';
+  const loginPage = new LoginPage(page);
+  await loginPage.go();
+  page.on('dialog', dialog => {
+    expect(dialog.message()).toBe(loginMessage);
+    dialog.dismiss();
+  });
+  await loginPage.login('incorrect', 'credential');
 })
